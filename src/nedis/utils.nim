20df8347ebbe46eea4c31891d9d28f138dc6399d
flywind <xzsflywind@gmail.com>
2019-11-10 19:55:08 +0800

    improve

--- src/nedis/utils.nim
+++ src/nedis/utils.nim
@@ -1,11 +1,11 @@
 type
-  SDS* = object
+  SDS* = ref object
     len: int
     free: int
     buf*: ptr UncheckedArray[char]
 
 proc newSDS*(initSize: int = 8): SDS = 
-  SDS(len: 0, free: initSize, buf: cast[ptr UncheckedArray[char]](alloc0(sizeof(char) * initSize)))
+  SDS(len: 0, free: initSize, buf: cast[ptr UncheckedArray[char]](alloc(sizeof(char) * initSize)))
 
 proc newSDS*(s: string, initSize: int = 8): SDS = 
   if s.len == 0:
@@ -19,15 +19,27 @@ proc avail*(s: SDS): int =
   s.free
 
 proc clone*(s: SDS): SDS = 
+  new result
   result.len = s.len
   result.free = s.free
-  result.buf = cast[ptr UncheckedArray[char]](alloc0(result.len))
+  result.buf = cast[ptr UncheckedArray[char]](alloc(result.len))
   for i in 0 ..< result.len:
     result.buf[i] = s.buf[i]
 
-let s = newSDS("hello, Nim!")
-let m = s.clone()
-echo m
+
+proc freeSDS*(s:var SDS) = 
+  dealloc(s.buf)
+  s.buf = nil
+  s = nil
+
+proc `$`*(s: SDS): string =
+  result = newString(s.len)
+  for i in 0 ..< s.len:
+    result[i] = s.buf[i] 
+
+var s = newSDS("hello, Nim!")
+freeSDS(s)
+echo s
 
 
   
